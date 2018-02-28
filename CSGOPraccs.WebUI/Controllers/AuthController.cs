c0baf39de94558f5b402060864bae9a902c232ff
Patryk Szylin <patrykszylin93@gmail.com>
2018-02-28 19:13:47 +0000

    Changed Web.Config to have : owin:AutomaticAppStartup command. It shouldn't even exist there... but it omits the problem when starting app. And also added Owin and Identity packages

---
+++ CSGOPraccs.WebUI/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSGOPraccs.WebUI.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        //public ActionResult Register()
+        //{
+
+        //}
+
+    }
+}
